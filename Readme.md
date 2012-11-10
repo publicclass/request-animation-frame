27e298f67bcf0cc9f30253f0047db17b32f4b3fe
Robert Sköld <robert@publicclass.se>
2012-11-10 12:35:51 +0100

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,24 @@
+
+# request-animation-frame
+
+  A polyfill for requestAnimationFrame.
+
+## Installation
+
+    $ component install publicclass/request-animation-frame
+
+## API
+  
+
+    var requestAnimationFrame = require('request-animation-frame');
+
+    function render(){
+      requestAnimationFrame(render);
+
+      console.log('yeah...')
+    }
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,10 @@
+var requestAnimationFrame = function(fn){ setTimeout(fn, 1000 / 60) };
+
+module.exports = typeof window != 'undefined'
+  ? window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.oRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    || requestAnimationFrame
+  : requestAnimationFrame;
